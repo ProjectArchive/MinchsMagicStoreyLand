e7202a7577e3c6557b95880c3f0648edbb685a01
dkearney <dkearney@ubuntu.(none)>
2011-03-27 19:09:39 -0400

    added in a quad chip and a capacitor, standardized some syntax

--- BreadboardComponent.py
+++ BreadboardComponent.py
@@ -19,9 +19,9 @@ class BreadboardComponent(object):
 class FixedBreadboardComponent(BreadboardComponent):
 	"""An abstraction of a breadboard component--but only
 	components that have a fixed size, like an op-amp.  Has attributes
-	that are user defined."""
+	that are user defined. """
 	
-	def __init__(self,width,height,pinList,attributes,referencePin,displayName,technicalName):
+	def __init__(self,width,height,pinList,attributes,displayName,technicalName,referencePin):
 		""" width is number columns, height is the
 		number of rows, pinlist is locations where the pins are, 
 		attributes are qualities (like resistance), location based
@@ -47,32 +47,52 @@ class VariableBreadboardComponent(BreadboardComponent):
 		self.radiusRange = radiusRange
 		
 	def __repr__(self):
-		return 'component of %d pins' % (len(self.pinList))
+		return 'component'
 		
 class Resistor(VariableBreadboardComponent):
-	"""A two pin resistor.  give it a reference point and a name"""
+	"""A two pin resistor having size range 1 pin to 20"""
 	
 	def __init__(self,resistance):
-		"""resistor having size range 1 pin to 20"""
+		"""give it a resistance"""
 		
-		referencePin = RelativeLocation()
-		displayName = 'Resistor'
-		secondPin = None
-		technicalName = 'R'+str(resistance)
 		radiusRange = (1,20)
 		attributes = resistance
+		displayName = 'Resistor'
+		technicalName = 'R%d' % resistance
+		referencePin = RelativeLocation()
+		secondPin = None
 		VariableBreadboardComponent.__init__(self,radiusRange,attributes,displayName,technicalName,referencePin)
 		self.resistance = resistance
 		
 	def __repr__(self):
-		return "%d ohm %s at %d,%d"  % (self.resistance,self.displayName,self.referencePin.xLoc,self.referencePin.yLoc)		
+		return "%g ohm %s at %d,%d"  % (self.resistance,self.displayName,self.referencePin.xLoc,self.referencePin.yLoc)		
+class Capacitor(VariableBreadboardComponent):
+	"""A two pin capacitor of size 1 to 20 pins"""
+	
+	def __init__(self,capacitance):
+		"""Give it a capacitance"""
+		
+		radiusRange = (1,20)
+		attributes = capacitance
+		displayName = 'Capacitor'
+		technicalName = 'C%d' % capacitance
+		referencePin = RelativeLocation()
+		secondPin = None
+		VariableBreadboardComponent.__init__(self,radiusRange,attributes,displayName,technicalName,referencePin)
+		self.capacitance = capacitance
+		
+	def __repr__(self):
+		return "%g farad %s at %d,%d" % (self.capacitance,self.displayName,self.referencePin.xLoc,self.referencePin.yLoc)
+
 class OpAmp(FixedBreadboardComponent):
-	"""An eight-pin op amp. give me a reference pin and a name"""
+	"""An eight-pin op amp. doesnt have any attributes.
+	We start counting pins at 1, like in the real world"""
 	
 	def __init__(self):
-		"""reference is location of bottom left pin"""
+		"""reference is location of bottom left pin
+		8 pins. bottom left is reference pin"""
 		
-		pin1 = RelativeLocation(0,0)
+		referencePin = RelativeLocation(0,0)
 		pin2 = RelativeLocation(1,0)
 		pin3 = RelativeLocation(2,0)
 		pin4 = RelativeLocation(3,0)
@@ -80,20 +100,53 @@ class OpAmp(FixedBreadboardComponent):
 		pin6 = RelativeLocation(1,3)
 		pin7 = RelativeLocation(2,3)
 		pin8 = RelativeLocation(3,3)
-		pinList=[pin1,pin2,pin3,pin4,pin5,pin6,pin7,pin8]
+		pinList=[referencePin,pin2,pin3,pin4,pin5,pin6,pin7,pin8]
 		attributes=None
 		displayName= 'OpAmp' 		#default
 		technicalName = 'OPA551'
 		
-		FixedBreadboardComponent.__init__(self,4,4,pinList,attributes,pin1,displayName,technicalName)
+		FixedBreadboardComponent.__init__(self,4,4,pinList,attributes,displayName,technicalName,referencePin)
 		
 	def __repr__(self):
 		return "%s at %d,%d"  % (self.displayName,self.referencePin.xLoc,self.referencePin.yLoc)		
+class QuadChip(FixedBreadboardComponent):
+	"""A four op-amp chip. we start counting pins
+	at 1, like in the circuit diagrams"""
+	
+	def __init__(self):
+		
+		referencePin = RelativeLocation()
+		pin2 = RelativeLocation(1,0)
+		pin3 = RelativeLocation(2,0)
+		pin4 = RelativeLocation(3,0)
+		pin5 = RelativeLocation(4,0)
+		pin6 = RelativeLocation(5,0)
+		pin7 = RelativeLocation(6,0)
+		pin8 = RelativeLocation(0,0)
+		pin9 = RelativeLocation(1,3)
+		pin10 = RelativeLocation(2,3)
+		pin11 = RelativeLocation(3,3)
+		pin12 = RelativeLocation(4,3)
+		pin13 = RelativeLocation(5,3)
+		pin14 = RelativeLocation(6,3)
+		pinList=[referencePin,pin2,pin3,pin4,pin5,pin6,pin7,pin8,pin9,pin10,pin11,pin12,pin13,pin14]
+		width = 7
+		height = 4
+		attributes = None
+		displayName = 'Quad Chip'
+		technicalName = 'HDL551'
+		FixedBreadboardComponent.__init__(self,width,height,pinList,attributes,displayName,technicalName,referencePin,)
+	
+	def __repr__(self):
+		return "%s at %d,%d" % (self.displayName,self.referencePin.xLoc,self.referencePin.yLoc)
 
 
 minch = OpAmp()
 storey = Resistor(100)
+art = Capacitor(.001)
+brad = QuadChip()
 
 print minch
 print storey
-
+print art
+print brad
