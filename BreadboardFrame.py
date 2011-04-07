22f0f3b1214acaee7b8c6ba9967f784f517bb9df
Cory Dolphin <wdolphin@Cory-Laptop.(none)>
2011-04-07 02:10:07 -0400

    updated BreadboardFrame

--- BreadboardFrame.py
+++ BreadboardFrame.py
@@ -1,23 +1,16 @@
 #! /usr/bin/env python
 from Tkinter import *
+from Breadboard import *
 import tkMessageBox
-
-class BreadboardCanvas(Canvas):
-	"""This is the GUI"""
-	def __init__(self,master):
-		"""Initialize yourself"""
-		"""Initialise the base class"""
-		Canvas.__init__(self,master,width=200,height=500)
-		self.pack()
 
 class BreadboardFrame(Frame):
 	"""This is the GUI"""
-	def __init__(self,master=None):
+	def __init__(self,breadBoard,master=None):
 		"""Initialize yourself"""
 		"""Initialise the base class"""
 		Frame.__init__(self,master)
-		"""Set the Window Title"""
 		self.master.title("Minch's Magic Storey Land!")
+		self.breadBoard = breadBoard
 		"""Display the main window"
 		with a little bit of padding"""
 		self.grid(padx=10,pady=10)
@@ -27,22 +20,20 @@ class BreadboardFrame(Frame):
 	def createWidgets(self):
 		"""Create all the widgets that we need"""
 		"""Create the Text"""
-		self.lbText = Label(self, text="Enter Text:")
-		self.lbText.grid(row=0, column=0)
-		"""Create the Entry, set it to be a bit wider"""
-		self.enText = Entry(self)
-		self.enText.grid(row=0, column=1, columnspan=3)
-		self.bbcanvas=BreadboardCanvas(self)
-		"""Create the Button, set the text and the 
-		command that will be called when the button is clicked"""
-		self.btnDisplay = Button(self, text="Display!", command=self.Display)
-		self.btnDisplay.grid(row=0, column=4)
+		self.breadBoardCanvas = BreadboardCanvas(self.breadBoard,master=self)
 		
 	def Display(self):
 		"""Called when btnDisplay is clicked, displays the contents of self.enText"""
 		tkMessageBox.showinfo("Text", "You typed: %s" % self.enText.get())
-		
+
+class BreadboardCanvas(object):
+	def __init__(self,breadBoard,master=None,width=600,height=200):
+		self.breadBoard = breadBoard
+		self.canvas = Canvas(master=master,width=width,height=height)
+		self.canvas.pack()
+	
 
 if __name__ == "__main__":
-	guiFrame = BreadboardFrame(Tk())
-	guiFrame.master.mainloop()
+	guiFrame = BreadboardFrame(Tk(),Breadboard())
+	
+	guiFrame.mainloop()
