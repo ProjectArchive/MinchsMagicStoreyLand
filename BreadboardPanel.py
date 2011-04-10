818a056ebf3907ae1e1c61e17d0d8ec0a8a1275b
wdolphin <wcdolphin@gmail.com>
2011-04-10 15:45:43 -0400

    a slighty branch in UI development, started working in wxPython. Current results are favorable. See BreadboardPanel.py

--- BreadboardPanel.py
+++ BreadboardPanel.py
@@ -10,27 +10,26 @@ class BreadboardPanel(wx.Panel):
 	def __init__(self, parent,breadBoard):
 		wx.Panel.__init__(self, parent)
 		self.breadBoard = breadBoard
-		vbox = wx.BoxSizer(wx.VERTICAL)
-		gs = wx.GridSizer(self.breadBoard.numRows, self.breadBoard.numColumns, 1, 1)
-		self.someDict = {}
+		self.gs = wx.GridSizer(self.breadBoard.numRows, self.breadBoard.numColumns, 1, 1) #gridsizer with numRows rows and numCols col, 1 px padding
+		self.bitmapToXY = {} #
 		emptyBitMap = wx.Image('res/blank_slot.png').Scale(15,15,quality=wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
 		openBitMap = wx.Image('res/open_slot.png').Scale(15,15,quality=wx.IMAGE_QUALITY_HIGH).ConvertToBitmap()
 		for y in range(self.breadBoard.numRows):	
 			for x in range(self.breadBoard.numColumns):
 				isFilled = breadBoard.getLocation(x,y).isFilled
-				print isFilled
-				if isFilled:
-					bmp =wx.StaticBitmap(self, -1, bitmap=emptyBitMap,size=(15,15),style = wx.NO_BORDER)
+				if isFilled: #different images. Should add support for flags, i.e. red, blue striples and always filled, etc.
+					bmp =wx.StaticBitmap(self, -1, bitmap=emptyBitMap,size=(15,15),style = wx.NO_BORDER) #-1 = no id, no border overrides default 3d bevel
 				else:
 					bmp =wx.StaticBitmap(self, -1, bitmap=openBitMap,size=(15,15),style = wx.NO_BORDER)
-				self.someDict[bmp] = (x,y)
-				bmp.Bind(wx.EVT_MOTION, self.onMotion,id=-1)
-				gs.Add(bmp,0)
-		print len(self.someDict.values())
-		vbox.Add(gs,flag=wx.EXPAND)
-		self.SetSizer(vbox)
+				self.bitmapToXY[bmp] = (x,y) #map this staticbitmap to a tuple of  x,y, location
+				bmp.Bind(wx.EVT_MOTION, self.onMotion,id=-1) #bind generic onMotion event,
+				self.gs.Add(bmp,0) #add to the grid sizer, with no id
+		print ('%d locations present') %len(self.someDict.values())
+		self.SetSizer(self.gs) #set this panel's sizer as the grid sizer
 	def onMotion(self,event):
-		print self.someDict.get(event.GetEventObject())
+		
+		x,y= self.someDict.get(event.GetEventObject())
+		print self.breadBoard.getLocation(x,y)
 		#print "motion event:", event.m_x, event.m_y
 
 		
