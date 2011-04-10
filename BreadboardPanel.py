83626edaa8a8b7b1ac0123f2a880f8aa857ef960
wdolphin <wcdolphin@gmail.com>
2011-04-10 15:53:17 -0400

    updated encoding and metadata to reflect actual heade.r Also added pretentious copyright header

--- BreadboardPanel.py
+++ BreadboardPanel.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# size.py
-
+#
+#       Part of Minch's Magic Storey Land 2011
+#
+#       Copyright 2011 Cory Dolphin <wcdolphin@gmail.com>       
 import wx
 from Breadboard import *
 
@@ -35,7 +36,7 @@ class BreadboardPanel(wx.Panel):
 
 		
 class Example(wx.Frame):
-  
+	"""Dummy frame"""
 	def __init__(self, parent, title):
 		super(Example, self).__init__(parent, title=title, 
 			size=(700, 300))
@@ -44,7 +45,7 @@ class Example(wx.Frame):
 
 
 if __name__ == '__main__':
-  
+	"""hey, this is how you incorporate testing into a module! @Dan @Noam"""
 	app = wx.App()
 	Example(None, title='Size')
 	app.MainLoop()
