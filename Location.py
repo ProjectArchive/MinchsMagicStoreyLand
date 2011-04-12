79696d269def5147ed77356043c9ab6959cc3baa
wdolphin <wcdolphin@gmail.com>
2011-04-11 21:12:38 -0400

    mhmm

--- Location.py
+++ Location.py
@@ -12,16 +12,26 @@ class Location(object):
 	CENTER_TOP = 4
 	CENTER_BOTTOM = 5
 	BLANK = 6
-	def __init__(self,xIn,yIn,displayFlag=Location.OPENSLOT):
+	
+	def __init__(self,xIn,yIn,displayFlag=OPENSLOT):
+		"""init Location, this location is at xIn,yIn with a display flag
+		 defaulted to an open slot, an empty location with no special
+		 display characteristics. 
+		 """
 		self.xLoc = xIn
 		self.yLoc = yIn
 		self.isFilled = False
 		self.Node = Node((xIn,yIn))  #Node takes a tuple of x and y
 		self.displayFlag = displayFlag
+	
 	def __repr__(self):
+		"""returns an informal representation of this Location """
 		suffix = '-Filled' if self.isFilled else '-Empty'
 		return str((self.xLoc, self.yLoc)) + suffix
+	
 	def setDisplayFlag(self,displayFlag):
+		"""Set display flag, determines the bitmap used to represent this
+		 locatio on the breadboard """
 		self.displayFlag = displayFlag
 		
 class RelativeLocation(Location):
@@ -30,7 +40,7 @@ class RelativeLocation(Location):
 	"""
 	
 	def __init__(self,xIn=0,yIn=0,referenceLocation=None):
-		""" Defaults create a REF RelativeLocation"""
+		""" Defaults create a REF RelativeLocation, no special display characteristics"""
 		self.referenceLocation = referenceLocation
 		Location.__init__(self,xIn,yIn)
 		
