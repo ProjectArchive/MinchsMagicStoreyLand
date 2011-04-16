aba127b63333d1504e4ed0ac222908d6a1dc7a1b
wdolphin <wcdolphin@gmail.com>
2011-04-16 13:33:58 -0400

    updated gui menu bar and events, reworked sizers

--- BreadboardGui.py
+++ BreadboardGui.py
@@ -6,7 +6,9 @@ class BreadboardGUI(wx.Frame):
 	def __init__(self, parent,breadBoard, *args, **kwargs):
 		wx.Frame.__init__(self, parent,*args,**kwargs)
 		self._mgr = wx.aui.AuiManager(self)
-		# create several text controls
+		# create menu
+		self.createMenu()
+		
 		self.breadBoardPanel = BreadboardPanel(self,breadBoard)
 
 		self.partBrowserPanel = wx.TextCtrl(self, -1, 'Part Browser Panel!',
@@ -24,7 +26,33 @@ class BreadboardGUI(wx.Frame):
 		self._mgr.Update()
 
 		self.Bind(wx.EVT_CLOSE, self.OnClose)
+	
+	def createMenu(self):
+		filemenu= wx.Menu()
+		
+		menuOpen = filemenu.Append(wx.ID_OPEN, "&Open","Generic open")
+		self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+		
+		menuAbout= filemenu.Append(wx.ID_ABOUT, "&About","Generic Information about this program")
+		self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+				
+		menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Get Minched")
+		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
+        # Creating the menubar.
+		menuBar = wx.MenuBar()
+		menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+		self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+	def OnExit(self,event):
+		print "on exit event, I killed the frame..."
+		self.OnClose(event) #for now, terminate frame
+	
+	def OnOpen(self,event):
+		print "on open event....we should do something"
+		
+	def OnAbout(self,event):
+		print "Something about me..."
 
 	def OnClose(self, event):
 		# deinitialize the frame manager
--- SimulationPanel.py
+++ SimulationPanel.py
@@ -10,12 +10,15 @@ from Breadboard import *
 class SimulationPanel(wx.Panel):
     def __init__(self, parent, *args, **kwargs):
  		wx.Panel.__init__(self, parent=parent,*args,**kwargs)
-		bSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.bHorizontalSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.bVerticalSizer = wx.BoxSizer(wx.VERTICAL)
 		image = wx.Image('res/simulate_image.png')
 		image.Rescale(50,50,wx.IMAGE_QUALITY_HIGH)
 		self.startSimulationButton = wx.BitmapButton(self, -1, wx.BitmapFromImage(image))
-		bSizer.Add(self.startSimulationButton,wx.ALIGN_CENTER)
-		self.SetSizer(bSizer)
+		self.bHorizontalSizer.Add(self.startSimulationButton,wx.ALIGN_CENTER)
+		self.bVerticalSizer.Add(self.bHorizontalSizer)
+		self.SetSizer(self.bVerticalSizer)
+		
 class Example(wx.Frame):
 	"""Dummy frame"""
 	def __init__(self, parent, title):
