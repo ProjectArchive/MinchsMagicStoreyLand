2dc31fc4f5aaff958f2271b5549df231f39d3b51
Dan Kearney <dkearney@SuzyHong.(none)>
2011-04-20 15:29:18 -0400

    can't put component off bb

--- Breadboard.py
+++ Breadboard.py
@@ -164,6 +164,8 @@ class Breadboard(object):
 		for i in range(0,len(pinPositions),2):
 			x = pinPositions[i]
 			y = pinPositions[i+1]
+			if x>self.numColumns or y>self.numRows:
+				return False
 			refLocTest = self.getLocation(x,y) #the loc to test at
 			#first test if the reference location is available
 			if refLocTest == None or refLocTest.isFilled:
@@ -177,7 +179,7 @@ class Breadboard(object):
 		return flag
 
 	def putComponent(self,aComponent,*args):
-		"""This function puts the a component down.Give it a reference pin for a regular component.
+		"""This function puts the a component down. Give it a reference pin for a regular component.
 		Give it x1,y1,x2,y2 for a variable size component, or x,y for an input device. """	
 		
 		if self.canPutComponent(aComponent,args):
@@ -224,8 +226,7 @@ class Breadboard(object):
 		
 	def clearBreadboard(self):
 		""" Removes all components from component list
-		unfills all pins
-		deletes all components from memory """
+		unfills all pins deletes all components from memory """
 		for component in self.componentList:
 			self.removeComponent(component)
 
@@ -243,7 +244,7 @@ class Breadboard(object):
 	def checkDistance(self,x,y,aComponent):
 		"""Makes sure we aren't stretching a component
 		beyond its maximum length"""
-		if isinstance(aComponent,FixedBreadboardComponent):
+		if isinstance(aComponent,VariableBreadboardComponent):
 			return (x**2 + y**2)**.5 > aComponent.maxLength
 		return True
 		
@@ -294,7 +295,7 @@ if __name__ == "__main__":
 	r = InputDevice(10)
 	
 	bb.putComponent(c,4,4,4,5)
-	bb.putComponent(d,5,4,5,5)
+	print bb.putComponent(d,5,4,5,50)
 	bb.putComponent(r,3,3)
 	print bb.componentList
 	
