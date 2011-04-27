d7b088eadab1f41c60c9e684ac7754e5978b20d7
unknown <nrubin@.olin.edu>
2011-04-27 16:57:37 -0400

    Added docstrings and cleaned the file up.

--- B2Spice.py
+++ B2Spice.py
@@ -9,14 +9,14 @@ from Voltage import *
 class B2Spice(object):
 	
 	def __init__(self,board):
+		"""initializes a B2Spice analysis object. Create some variable names
+			that other methods need access to, along with some file persistence
+			names. Also calls some methods used for debugging (currently suppressed)"""
 		self.board = board
 		self.cirName = 'CIRCUIT%d' % id(self)
 		self.fileName = '%s.cir' % self.cirName
 		self.resName = '%s_res' % self.cirName
 		self.getNodesForPlot()
-		#~ os.system('mkdir b2spice')
-		#~ os.system('cd b2spice')
-		#~ self.clearEmptyNodes()
 		self.inputDeviceList = self.getInputDevices()
 		self.getInputDevices()
 		self.rails = self.getRails()
@@ -30,6 +30,7 @@ class B2Spice(object):
 		self.nodePlotDict[3] = 3
 		
 	def getRails(self):
+		"""returns the voltage at every rail on the breadboard"""
 		##The 0th value of the list is the bottom of the board
 		topRail = self.board.rails[0]
 		midTopRail = self.board.rails[1]
@@ -38,16 +39,19 @@ class B2Spice(object):
 		return (topRail,midTopRail,midBotRail,botRail)
 		
 	def makeRailCards(self):
+		""""makes the SPICE cards for each rail (a DC source)"""
 		railCount = 1;
 		railCards = ''
 		for item in self.rails:
 			if item != 0:
 				railCards += 'V%d %g 0 dc %g \n' % (railCount,railCount,item)
-				#~ railCards += 'V' + str(railCount) + ' 0 ' + str(railCount) + ' dc ' + str(item) + '\n'
 				railCount += 1
 		return railCards
 		
 	def getInputDevices(self):
+		"""Creates a list of Input Device objects so B2Spice knows exactly
+		what's providing voltage/current to the circuit. Does not include
+		incoming voltage from voltage rails."""
 		inputDeviceList = []
 		for comp in self.board.componentList:
 			if comp.displayName=='Input Device':
@@ -55,19 +59,21 @@ class B2Spice(object):
 		return inputDeviceList
 	
 	def makeInputDeviceCards(self):
+		"""grabs the list of input devices from getInputDevices and makes a SPICE card for each device,
+		based on the characteristics of that device. Only supports voltage sources"""
 		if len(self.inputDeviceList) <1:
 			return ''
 		inputDeviceCards = ''
 		for item in self.inputDeviceList:
 			if item.attributes['Voltage Type'] == 'AC':
 				inputDeviceCards += 'V%d %g 0 ac %g sin \n' % (id(item),item.pinList[0].Node.number,item.pinList[0].Node.voltage.volts)
-				#~ inputDeviceCards += 'V' + str(id(item)) + ' ' + str(item.pinList[0].Node.number) + ' 0 ' + 'sin \n'
 			else:
 				inputDeviceCards += 'V%d %g 0 dc %g \n' % (id(item),item.pinList[0].Node.number,item.voltage.volts)
-				#~ inputDeviceCards += 'V' + str(id(item)) + ' ' + str(item.pinList[0].Node.number) + ' 0 ' + 'dc ' + str(item.voltage.volts) + ' \n'
 		return inputDeviceCards
 
 	def makeAnalysisCards(self,analysisType='tran',scopedNode=0,vMin=0,vMax=5,tstep=.01,ttotal=1,stepType='lin',numSteps=20,startFreq=.001,endFreq=100000):
+		"""takes arguments for the type of analysis SPICE needs to go, with multiple default arguments, and makes the SPICE analysis cards for the chosen type
+		of analysis. SPICE automatically takes care of which source to vary, depending on the type of analysis."""
 		if len(self.inputDeviceList) <1:
 			return '.dc V1 %g %g 1 V2 %g %g 1 V3 %g %g 1\n.print dc v(%d) \n' % (self.board.rails[1],self.board.rails[1],self.board.rails[2],self.board.rails[2],self.board.rails[3],self.board.rails[3], scopedNode)
 		if analysisType == 'ac':
@@ -79,6 +85,8 @@ class B2Spice(object):
 			return self.makeTranCards(scopedNode,tstep,ttotal)
 		
 	def makeACCards(self,scopedNode,stepType,numSteps,startFreq,endFreq):
+		"""given an argument of AC analysis, this method creates the SPICE card that tells SPICE
+		to do AC analysis on a given node and print the result."""
 		if len(self.inputDeviceList) <1:
 			raise NameError('There are no input devices...')
 		acLine = '.ac %s %g %g %g \n' % (stepType,numSteps,startFreq,endFreq)
@@ -86,6 +94,8 @@ class B2Spice(object):
 		return acLine + printLine
 	
 	def makeDCCards(self,scopedNode,vMin,vMax,numSteps):
+		"""given an argument of DC analysis, this method creates the SPICE card that tells SPICE
+		to do DC analysis on a given node and print the result."""
 		dcInputDeviceList = []
 		for item in self.inputDeviceList:
 			if item.voltageType == 'DC':
@@ -97,6 +107,8 @@ class B2Spice(object):
 		return dcLine+printLine
 	
 	def makeTranCards(self,scopedNode,tstep,ttotal):
+		"""given an argument of transient analysis, this method creates the SPICE card that tells SPICE
+		to do transient analysis on a given node and print the result."""
 		tranLine = '.tran %g %g \n' % (tstep, ttotal)
 		printLine = '.print tran v(%d) \n' % scopedNode
 		return tranLine+printLine
@@ -106,7 +118,6 @@ class B2Spice(object):
 		if it's only one, and not power, then we take action and remove it"""
 		d={}
 		for component in self.board.componentList:
-			#~ if not isinstance(component,FixedBreadboardComponent):
 			for pin in component.pinList:
 				d[pin.Node.number]=d.get(pin.Node.number,0)+1
 		for val in d:
@@ -149,6 +160,8 @@ class B2Spice(object):
 		return str(attrKey),str(attrVal)
 					
 	def buildVariableComponentText(self,component):
+		"""creates the SPICE cards for Resistors, Capacitors and Inductors. If
+		the component is a capactior, appends the card with an initial condition."""
 		if isinstance(component,Capacitor):
 			suffix = ' ic=0'
 		else:
@@ -161,7 +174,7 @@ class B2Spice(object):
 		
 	def buildOpAmpText(self,opamp):
 		"""returns a tuple describing the nodal location of the opamp with the spice file,
-		and a big line of text containing the definition of the opamp"""
+		and a big line of text containing the definition of the opamp, pulled from a persistent file OPA551.txt"""
 		pinDict = {}
 		pinDict['notUsed1'] = opamp.pinList[0]
 		pinDict['negIn'] = opamp.pinList[1]
@@ -180,11 +193,12 @@ class B2Spice(object):
 		opAmpCard = '%s %s %s\n' % (opAmpID,opAmpNodeString,subCktID)
 		fin.close()
 		return (opAmpCard,opAmpSubCkt)
-		#~ return 'e%d %d 0 %d %d 999k\n' % (id(opamp),pinDict['out'].Node.number,pinDict['plusIn'].Node.number,pinDict['negIn'].Node.number)
 	
 	def buildNetList(self,analysisType='tran',scopedNode=0,vMin=0,vMax=5,tstep=.01,ttotal=1,stepType='lin',numSteps=20,startFreq=.001,endFreq=100000):
-		"""dc requires scopedNode,vMin,vMax,and stepSize. tran requires scopedNode, tstep, and ttotal. ac requires stepType, numSteps,
-		startFreq, and endFreq"""
+		"""Creates the necessary files for analysis, and then conducts the anaylsis based on input arguments from the GUI (all are defaulted). Builds the full net list
+		for SPICE, using above methods. Writes the full net list to a text file, then uses Subprocess to communicate with ngspice and simulate the circuit. Writes the 
+		simulation results to a file, which the GUI grapher then opens. Returns the resulting file name.
+		"""
 		makeFileCommand = 'touch %s' % self.fileName
 		makeFileCommand2 = 'touch %s' % self.resName
 		os.system(makeFileCommand)
@@ -199,7 +213,6 @@ class B2Spice(object):
 				netList += self.buildVariableComponentText(comp)
 				self.nodePlotDict[nodePlotCount] = comp.pinList[0].Node.number
 				nodePlotCount += 1
-				#~ compCard,subCktCard = self.buildOpAmpText(comp)
 			elif isinstance(comp,OpAmp):
 				compCard,subCktCard = self.buildOpAmpText(comp)
 				netList += compCard
@@ -216,36 +229,23 @@ class B2Spice(object):
 			netList += self.makeAnalysisCards('ac',scopedNode=scopedNode,stepType=stepType,numSteps=numSteps,startFreq=startFreq,endFreq=endFreq)
 		if analysisType == 'tran':
 			netList += self.makeAnalysisCards('tran',scopedNode=scopedNode,tstep=tstep,ttotal=ttotal)
-		#~ netList += 'write %s allv\n' % self.resName
 		netList += '.end'
 		self.netList = netList
 		#File interface stuff
 		fout = open(self.fileName,'w')
 		fout.write(self.netList)
 		fout.close()
-		#~ spiceCommand = 'ngspice -b %s -r %s' % (self.fileName,self.resName)
-		#~ print self.nodeList
 		res = subprocess.Popen(['ngspice','-b',self.fileName,'-r',self.resName],stdout=subprocess.PIPE).communicate()[0]
-		#~ p = subprocess.Popen(['ngspice'],stdout=subprocess.PIPE)
-		#~ p.communicate('set filetype=ascii')
-		#~ buildCirLine = 'build %s -r %s' % (self.fileName,self.resName)
-		#~ res = p.communicate(buildCirLine)[0]
-		#~ p.terminate()
-		#~ p.communicate('set filetype=ascii')
-		#~ res = p.communicate()[0]
-		#~ ,'-r',self.resName
-		#~ res = os.system(spiceCommand)
 		delFileCommand = 'rm %s' % self.fileName
 		os.system(delFileCommand)
 		subprocess.Popen(['gwave',self.resName],stdout=subprocess.PIPE).communicate()[0]
-		#~ print self.netList
 		return self.resName
 	
 	def scopeAnalysis(self):
 		"""searches for the scope or scopes,
 		returns list of nodes at which the scops) live.
-		also removes the scope from bb component list
-		because we dont want to do analysis"""
+		also removes the scope from breadboard component list
+		because we dont want to do analysis with them in the list"""
 		
 		scopeNodes =[]
 		for i in range(len(bb.componentList)):
@@ -259,6 +259,7 @@ class B2Spice(object):
 		
 		
 if __name__ == '__main__':
+	#Yay for debugging!!
 	bb = Breadboard()
 	source = InputDevice(.01,'DC')
 	p = OpAmp()
@@ -276,9 +277,7 @@ if __name__ == '__main__':
 	print bb.putComponent(C,5,14,3,16)
 	b = B2Spice(bb)
 	print b.nodeList
-	#~ print b.rails
 	print b.buildNetList('tran',scopedNode=25,tstep = .001,ttotal=1)
-	#~ print b.buildNetList('ac',scopedNode=5,stepType='dec',numSteps = 20,startFreq=.0001,endFreq=1000)
 	print b.netList
 	print b.nodePlotDict
 	
